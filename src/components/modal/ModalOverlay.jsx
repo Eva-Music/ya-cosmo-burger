ec95a5d6ae513860bbe9ae3eb494e6dfb1b86453
Машкова Ольга Игоревна <Mashkova.O.I@sberbank.ru>
2021-08-19 16:30:02 +0300

    modaloverlay fix

--- src/components/modal/ModalOverlay.jsx
+++ src/components/modal/ModalOverlay.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useRef} from "react";
 import styles from "./modal-overlay.module.css"
+import PropTypes from "prop-types";
 
-const ModalOverlay = () => {
+const ModalOverlay = ({onClose}) => {
+    const ref = useRef();
+
+    const handleClickOutside = (e) => {
+        if (ref && ref.current && ref.current.contains(e.target)) {
+            onClose();
+        }
+    };
 
     return (
-        <div className={styles.layout}>
+        <div onClick={handleClickOutside} ref={ref} className={styles.overlay}>
         </div>
     );
+}
+
+ModalOverlay.propTypes = {
+    onClose: PropTypes.func.isRequired
 }
 
 export default ModalOverlay;
